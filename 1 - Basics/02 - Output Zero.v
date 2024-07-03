d865d5c8c92b31f44e7d6e2b1590470b98bd86af
JasonHsiao8 <jasonhsiao2019@gmail.com>
2024-07-03 14:58:15 +0800

    Create 02 - Output Zero.v

---
+++ 1 - Basics/02 - Output Zero.v
@@ -0,0 +1,4 @@
+module top_module(output zero);// Module body starts after semicolon
+    //assign allows to drive an output continuously with a value
+	assign zero = 0; //bits' decimal value
+endmodule
