5b8d45180354b7bb82a0a330383a15c1f54e8f2a
JasonHsiao8 <jasonhsiao2019@gmail.com>
2024-07-11 17:56:24 +0800

    Create 05 - Combination for-loop: 255-bit population count.v

---
+++ 5 - More Verilog Features/05 - Combination for-loop: 255-bit population count.v
@@ -0,0 +1,18 @@
+module top_module( 
+    input [254:0] in,
+    output [7:0] out );
+
+    integer i;
+    reg [7:0]count;
+    always @(*)begin
+        count = 0;
+    	for(i = 0; i < 255; i = i + 1)begin
+            if(in[i] == 1'b1)
+                count = count + 1'b1;
+            else 
+                count = count;
+    	end
+        out = count;
+    end
+endmodule
+
