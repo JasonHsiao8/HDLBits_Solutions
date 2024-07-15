aee9231e35a6c86461b9d023a38b6c4de16da48b
JasonHsiao8 <jasonhsiao2019@gmail.com>
2024-07-15 16:16:35 +0800

    Create 15 - 3-bit population count.v

---
+++ 6 - Combinational Circuit/1 - Basic Gate/15 - 3-bit population count.v
@@ -0,0 +1,43 @@
+module top_module( 
+    input [2:0] in,
+    output [1:0] out );
+
+    integer i, count;
+    always @(*)begin
+        count = 0;
+        for(i = 0; i < 3; i++)begin
+            if(in[i] == 1'b1) count = count + 1;
+        end
+    end
+    assign out = count;
+endmodule
+	// This is a function of 3 inputs. One method is to use a 8-entry truth table:
+	// in[2:0] out[1:0]
+	// 000      00
+	// 001      01
+	// 010      01
+	// 011      10
+	// 100      01
+	// 101      10
+	// 110      10
+	// 111      11
+
+	// Using the addition operator works too:
+	// assign out = in[0]+in[1]+in[2];
+	
+	// Yet another method uses behavioural code inside a procedure (combinational always block)
+	// to directly implement the truth table:
+	/*
+	always @(*) begin
+		case (in)
+			3'd0: out = 2'd0;
+			3'd1: out = 2'd1;
+			3'd2: out = 2'd1;
+			3'd3: out = 2'd2;
+			3'd4: out = 2'd1;
+			3'd5: out = 2'd2;
+			3'd6: out = 2'd2;
+			3'd7: out = 2'd3;
+		endcase
+	end
+	*/
