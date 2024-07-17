36f1e7c171c8cbadfa5d2b66ab16cc50e5fb8be5
JasonHsiao8 <jasonhsiao2019@gmail.com>
2024-07-18 00:17:20 +0800

    Update 01 - Modules.v

--- 3 - Modules/01 - Modules.v
+++ 3 - Modules/01 - Modules.v
@@ -1,6 +1,6 @@
 module top_module ( input a, input b, output out );
 	//module mod_a ( input in1, input in2, output out );
-    // 模塊內容
+    // module content
 	//endmodule
   mod_a instance1(.in1(a), .in2(b), .out(out)); // a, b, out為來自module外部的信號
 
